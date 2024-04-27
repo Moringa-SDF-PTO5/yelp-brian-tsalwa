f399afdaa0414a5280bdbf4b96beb5c72571baad
btsalwa <brian.tsalwa@student.moringaschool.com>
2024-04-27 20:17:10 +0300

    customer property returns first & last name

--- app.py
+++ app.py
@@ -9,24 +9,32 @@ class Customer():
         self.last_name = last_name
         self._customer_objects.append(self)
 
-  
-class Restaurant:
-    all = []
-    def __init__(self, name, ):
-        self.name = name
-        if not (isinstance(name, str)):
-            raise ValueError("name must be a string")
-        if not (1 <= len(name) <= 100 ):
-            raise ValueError("name must not be greater than 100")
     @property
     def name(self):
-        return 
+        return self.first_name + self.last_name
+    @name.setter
+    def name(self, rating):
+        
+    
+
+  
+# class Restaurant:
+#     all = []
+#     def __init__(self, name, ):
+#         self.name = name
+#         if not (isinstance(name, str)):
+#             raise ValueError("name must be a string")
+#         if not (1 <= len(name) <= 100 ):
+#             raise ValueError("name must not be greater than 100")
+#     @property
+#     def name(self):
+#         return self.name.append(Customer)
 
 
 
 
-class Review:
-    def __init__(self, customer, restaurant, rating):
-        self.customer = customer
-        self.restaurant = restaurant
-        self.rating = rating
+# class Review:
+#     def __init__(self, customer, restaurant, rating):
+#         self.customer = customer
+#         self.restaurant = restaurant
+#         self.rating = rating
