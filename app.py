93b4789d52ec2f2ca493d86a99185a434b1a482b
btsalwa <brian.tsalwa@student.moringaschool.com>
2024-04-28 17:06:49 +0300

    class review def self ratring,restaurant,customer

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 class Customer():
-    all = []
+    all_customers = []
     def __init__(self, first_name, last_name):
         if not (isinstance(first_name, str) and isinstance(last_name, str)):
             raise ValueError("Names must be of type str")
@@ -8,31 +8,28 @@ class Customer():
         self.first_name = first_name
         self.last_name = last_name
         self.full_name = f"{first_name} {last_name}"
-        self.all.append(self)
+        self.all_customers.append(self)
 
-
-    @property
-    def name(self):
-        return self.full_name
 
 
 # first_name = "Brian"
 # last_name = "Tsalwa"
 # customer1 = Customer(first_name, last_name)
 
-# print(customer1.name)
+# print(customer1.full_name)
 
 
 class Restaurant:
-    all = []
+    all_restaurants = []
     def __init__(self, name):
         if not (isinstance(name, str)):
             raise ValueError("name must be a string")
         if not (1 <= len(name) <= 100):
             raise ValueError("name must not be greater than 100 characters")
+        
+        
         self._name = name
-        self.all.append(self)
-
+        self.all_restaurants.append(self)
 
     @property
     def name(self):
@@ -45,27 +42,7 @@ class Restaurant:
         if not (1 <= len(value) <= 100):
             raise ValueError("name must not be greater than 100 characters")
         self._name = value
-
-# name = "Sushi"
-# restaurant1 = Restaurant(name)
-
-# print(restaurant1.name)
-    
-   
-   
-
-
-
-
-class Review:
-    def __init__(self, customer, restaurant, rating):
-        if not (isinstance(customer, Customer) and isinstance(restaurant, Restaurant)):
-            raise ValueError("customer and restaurant must be instances of Customer and Restaurant classes")
-        if not (isinstance(rating, int) and 1 <= rating <= 5):
-            raise ValueError("rating must be an integer between 1 and 5, inclusive")
-        self.customer = customer
-        self.restaurant = restaurant
-        self._rating = rating
+        if not (isinstance(value, Customer(uniqu)))
 
     @property
     def customer(self):
@@ -73,21 +50,33 @@ class Review:
 
     @customer.setter
     def customer(self, value):
-        if not isinstance(value, Customer):
-            raise ValueError("customer must be an instance of the Customer class")
+        if not (isinstance(value, Customer)):
+            raise ValueError("customer must be an instance of Customer class")
         self._customer = value
 
     @property
-    def restaurant(self):
-        return self._restaurant
+    def reviews(self):
+        return self._reviews
 
-    @restaurant.setter
-    def restaurant(self, value):
-        if not isinstance(value, Restaurant):
-            raise ValueError("restaurant must be an instance of the Restaurant class")
-        self._restaurant = value
+    @reviews.setter
+    def reviews(self, value):
+        if not (isinstance(value, list)):
+            raise ValueError("reviews must be a list")
+        self._reviews = value
 
-    @rating.setter
-    def rating(self, value):
-        raise AttributeError("rating attribute cannot be changed after the Review object is initialized")
-        self.rating = value
+# name = "Sushi"
+# restaurant1 = Restaurant(name)
+
+# print(restaurant1.name)
+
+class Review:
+
+    def __init__(self, id: int, rating: int, customer: 'Customer', restaurant: 'Restaurant'):
+
+        self.id = id
+
+        self.rating = rating
+
+        self.customer = customer
+
+        self.restaurant = restaurant
